0ffe4d28df2144fc13bb8d96b23d13dc0b64b6a0
AdrianPaoletti <adriandelapao@gmail.com>
2021-11-12 20:33:25 +0100

    Added athoritation meddleware

---
+++ server/middlewares/auth.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+
+const Auth = (req, res, next) => {
+  console.log("dentroAuth");
+  const authHeader = req.header("Authorization");
+  console.log(authHeader);
+  if (!authHeader) {
+    const error = new Error("Nono t no register");
+    error.code = 401;
+    next(error);
+  } else {
+    const token = authHeader.split(" ")[1];
+    console.log(token)
+    if (!token) {
+      const error = new Error("Token incorrect");
+      error.code = 401;
+      next(error);
+    } else {
+      try {
+        const user = jwt.verify(token, process.env.SECRET);
+        req.userId = user.id;
+        next();
+      } catch {
+        const error = new Error("Token incorrecto");
+        error.code = 401;
+        next(error);
+      }
+    }
+  }
+}
+
+module.exports = Auth;
