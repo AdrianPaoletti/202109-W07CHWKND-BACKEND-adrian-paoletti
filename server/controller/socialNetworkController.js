3f706cd17fc68519cb712e7b369c61dea81f1677
AdrianPaoletti <adriandelapao@gmail.com>
2021-11-13 17:50:50 +0100

    Added update function

--- server/controller/socialNetworkController.js
+++ server/controller/socialNetworkController.js
@@ -42,6 +42,26 @@ const getFriends = async (req, res, next) => {
   }
 }
 
+const getEnemies = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ _id: req.userId })
+      .populate({
+        path: "enemies"
+      })
+    if (user) {
+      res.json(user.enemies)
+    } else {
+      const error = new Error("Could not get userEnemies");
+      error.code = 404;
+      next(error);
+    }
+  } catch (error) {
+    error.code = 400;
+    error.message = "General pete userEnemies";
+    next(error);
+  }
+}
+
 const addFriend = async (req, res, next) => {
   try {
     const friend = req.body;
@@ -99,9 +119,88 @@ const removeFriend = async (req, res, next) => {
   }
 }
 
+const addEnemy = async (req, res, next) => {
+  try {
+    const enemy = req.body;
+    const enemyToAdd = await User.findOne({ "username": enemy.username });
+    if (enemyToAdd) {
+      const user = await User.findOne({ _id: req.userId });
+      if (user) {
+        user.enemies = [...user.enemies, enemyToAdd._id];
+        await user.save(user);
+        res.json(enemyToAdd);
+      }
+      else {
+        const error = new Error("Could not find user to add enemie");
+        error.code = 404;
+        next(error);
+      }
+    } else {
+      const error = new Error("Could not find enemie");
+      error.code = 404;
+      next(error);
+    }
+  } catch (error) {
+    error.code = 400;
+    error.message = "General pete on addEnemie";
+    next(error);
+  }
+}
+
+const removeEnemy = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const enemyToRemove = await User.findOne({ _id: id });
+    if (enemyToRemove) {
+      const user = await User.findOne({ _id: req.userId });
+
+      if (user) {
+        user.enemies = user.enemies.filter((friend) => friend._id.toString() !== enemyToRemove._id.toString())
+        await user.save(user);
+        res.json(user.enemies);
+      }
+      else {
+        const error = new Error("Could not find user to remove enemy");
+        error.code = 404;
+        next(error);
+      }
+    } else {
+      const error = new Error("Could not find enemy");
+      error.code = 404;
+      next(error);
+    }
+  } catch (error) {
+    error.code = 400;
+    error.message = "General pete on removeEnemy";
+    next(error);
+  }
+}
+
+const updateUser = async (req, res, next) => {
+  try {
+    // eslint-disable-next-line dot-notation
+    const updatedUser = await User.findByIdAndUpdate(req.body['_id'], req.body, { new: true });
+    if (updatedUser) {
+      res.json(updatedUser);
+    }
+    else {
+      const error = new Error("Not posibol to update");
+      error.code = 404;
+      next(error);
+    }
+  } catch (error) {
+    error.code = 400;
+    next(error);
+  }
+}
+
 module.exports = {
   getUsers,
   getFriends,
   addFriend,
   removeFriend,
+  addEnemy,
+  removeEnemy,
+  getEnemies,
+  updateUser,
 }
