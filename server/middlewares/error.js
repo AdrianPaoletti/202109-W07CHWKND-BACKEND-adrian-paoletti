2520cf7d7edd615314d60589a93d980726aa390f
AdrianPaoletti <adriandelapao@gmail.com>
2021-11-14 14:00:22 +0100

    Error endopoint added

--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -1,12 +1,17 @@
 const debug = require("debug")("socialMedia:error");
+const { ValidationError } = require("express-validation");
 
 const notFoundErrorHandler = (req, res) => {
   res.status(404).json({ error: "Not Endpoint found" });
 }
 
 const generalErrorHandler = (error, req, res, next) => {
-  debug("Error: ", error.message);
-  const message = error.code ? error.message : "Holistic error";
+  debug(`Some error happens: ${error.message}`);
+  if (error instanceof ValidationError) {
+    error.code = 400;
+    error.message = "Bad request";
+  }
+  const message = error.code ? error.message : "General pete";
   res.status(error.code || 500).json({ error: message });
 };
 
