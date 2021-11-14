026da2c99d724d200e923b656ccfe955acc02bde
AdrianPaoletti <adriandelapao@gmail.com>
2021-11-14 13:50:49 +0100

    features/her

--- server/routes/socialNetworkRoutes.js
+++ server/routes/socialNetworkRoutes.js
@@ -1,15 +1,16 @@
 const express = require("express");
+const auth = require("../middlewares/auth")
 const { getUsers, getFriends, addFriend, removeFriend, addEnemy, removeEnemy, getEnemies, updateUser } = require("../controller/socialNetworkController")
 
 const router = express.Router();
 
-router.get("/", getUsers);
-router.get("/friends", getFriends)
-router.post("/friends/add", addFriend)
-router.delete("/friends/:id", removeFriend)
-router.get("/enemies", getEnemies)
-router.post("/enemies/add", addEnemy)
-router.delete("/enemies/:id", removeEnemy)
-router.put("/update", updateUser);
+router.get("/", auth, getUsers);
+router.get("/friends", auth, getFriends)
+router.post("/friends/add", auth, addFriend)
+router.delete("/friends/:id", auth, removeFriend)
+router.get("/enemies", auth, getEnemies)
+router.post("/enemies/add", auth, addEnemy)
+router.delete("/enemies/:id", auth, removeEnemy)
+router.put("/update", auth, updateUser);
 
 module.exports = router;
